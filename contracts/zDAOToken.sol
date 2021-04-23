d9fe90113898fa4b72d97ee4d86d10e75b3ef65b
Zachary <remscar@gmail.com>
2021-04-23 00:01:06 -0700

    Added a gas efficient bulk transfer method

--- contracts/zDAOToken.sol
+++ contracts/zDAOToken.sol
@@ -1,10 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.3;
 
+/**
+ * Modifications from base ERC20 token:
+ *  > mapping(address => uint256) _balances
+ *       is now internal instead of private.
+ */
+import "./oz/ERC20Upgradeable.sol";
+import "./oz/ERC20SnapshotUpgradeable.sol";
+import "./oz/ERC20PausableUpgradeable.sol";
+
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20SnapshotUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PausableUpgradeable.sol";
 
 contract ZDAOToken is
   OwnableUpgradeable,
@@ -22,8 +28,53 @@ contract ZDAOToken is
     __ERC20Pausable_init();
   }
 
+  /**
+   * Mints new tokens. Minting logic is delegated to the owner of this contract.
+   * @param account the account to mint the tokens for
+   * @param amount the amount of tokens to mint.
+   */
   function mint(address account, uint256 amount) external onlyOwner {
     _mint(account, amount);
+  }
+
+  /**
+   * Utility function to transfer tokens to many addresses at once.
+   * @param recipients The addresses to send tokens to
+   * @param amount The amount of tokens to send
+   * @return Boolean if the transfer was a success
+   */
+  function transferBulk(address[] calldata recipients, uint256 amount)
+    external
+    returns (bool)
+  {
+    address sender = _msgSender();
+
+    uint256 total = amount * recipients.length;
+    require(
+      _balances[sender] >= total,
+      "ERC20: transfer amount exceeds balance"
+    );
+
+    require(!paused(), "ERC20Pausable: token transfer while paused");
+
+    _balances[sender] -= total;
+    _updateAccountSnapshot(sender);
+
+    for (uint256 i = 0; i < recipients.length; ++i) {
+      address recipient = recipients[i];
+      require(recipient != address(0), "ERC20: transfer to the zero address");
+
+      // Note: _beforeTokenTransfer isn't called here
+      // This function emulates what it would do (paused and snapshot)
+
+      _balances[recipient] += amount;
+
+      _updateAccountSnapshot(recipient);
+
+      emit Transfer(sender, recipient, amount);
+    }
+
+    return true;
   }
 
   function _beforeTokenTransfer(
