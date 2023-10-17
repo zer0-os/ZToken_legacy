367d6f825ebac39140209ef3a5b6ba066dc5926b
Stefan Bancu <Stefan>
2023-10-17 19:05:24 +0300

    added tests for storage variable

--- test/forkMeowUpgrade.ts
+++ test/forkMeowUpgrade.ts
@@ -3,6 +3,7 @@ import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import * as hre from "hardhat";
 import { MeowToken, MeowToken__factory } from "../typechain";
+import {ethers} from "ethers";
 
 describe("MeowToken", () => {
 
@@ -16,9 +17,14 @@ describe("MeowToken", () => {
   const tokenAddress = "0x0ec78ed49c2d27b315d462d43b5bab94d2c79bf8";
   const multisigAddress = "0x5eA627ba4cA4e043D38DE4Ad34b73BB4354daf8d";
   const proxyAdmin = "0x5dc79cf30bdc7ead0afd107f3ab3494fb666b86c";
+  let contract;
+  let snapshotIdBeforeUpgrade;
+  let snapshotIdAfterUpgrade;
 
 
   before(async () => {
+    contract = await hre.ethers.getContractAt("MeowToken", tokenAddress);
+    snapshotIdBeforeUpgrade = await hre.network.provider.send("evm_snapshot");
     await hre.network.provider.request({
       method: "hardhat_impersonateAccount",
       params: [multisigAddress],
@@ -73,6 +79,7 @@ describe("MeowToken", () => {
       "0x1000000000000000000000",
     ]);
     user3 = await hre.ethers.getSigner("0x94F91B1E0c994F3Cca868Db2Fd24cc5d526928Ac");
+    snapshotIdAfterUpgrade = await hre.network.provider.send("evm_snapshot");
 
 
   });
@@ -80,7 +87,6 @@ describe("MeowToken", () => {
 
   describe("new implementation tests", async () => {
     it("set of tests", async () => {
-      const contract = await hre.ethers.getContractAt("MeowToken", tokenAddress);
 
       const initialBalance = await contract.totalSupply();
 
@@ -98,7 +104,6 @@ describe("MeowToken", () => {
 
 
     it("Test transferFromBulk", async () => {
-      const contract = await hre.ethers.getContractAt("MeowToken", tokenAddress);
 
       const initialBalance = await contract.totalSupply();
       const initialUser1Balance = await contract.balanceOf(user1.address);
@@ -118,6 +123,42 @@ describe("MeowToken", () => {
       expect(await contract.totalSupply()).to.be.equal(initialBalance.sub(1000));
       expect(await contract.balanceOf(user2.address)).to.be.equal(initialUser2Balance.add(1000));
       expect(await contract.balanceOf(user1.address)).to.be.equal(initialUser1Balance.sub(2000));
+
+    });
+    it("check storage variables after upgrade", async () => {
+      const randomHolders = [
+        "0xEf147697d948D609F712397Db270234CF155A925",
+        "0x766aBC6f937ad9248752A9f3ee8e850755D1438A",
+        "0x58DEd8069032DA31CA975D2E0e44a788B71A6e8c",
+        "0x1789C3abbDfa049374eE9c6CB37C93833Ae5D8Ea",
+        "0x1e11FCAB5BD387bc783F3DD03b379402ee2f9e06",
+        "0x0004Cd8474E882278e32E584699090be496f410e",
+        ];
+      const randomHolderAmounts = [];
+      await hre.network.provider.send("evm_revert", [snapshotIdBeforeUpgrade]);
+      const totalSupply = await contract.totalSupply();
+
+      const decimals = await contract.decimals();
+      const name = await contract.name();
+      const symbol = await contract.symbol();
+      const owner = await contract.owner();
+
+      for (let i=0; i<randomHolders.length; i++) {
+        randomHolderAmounts.push(await contract.balanceOf(randomHolders[i]));
+      }
+      await hre.network.provider.send("evm_revert", [snapshotIdAfterUpgrade]);
+      const randomHoldersAfterUpgrade = [];
+
+      for (let i=0; i<randomHolders.length; i++) {
+        randomHoldersAfterUpgrade.push(await contract.balanceOf(randomHolders[i]));
+      }
+      expect(await contract.totalSupply()).to.be.equal(totalSupply);
+      expect(await contract.decimals()).to.be.equal(decimals);
+      expect(await contract.name()).to.be.equal(name);
+      expect(await contract.symbol()).to.be.equal(symbol);
+      expect(await contract.owner()).to.be.equal(owner);
+      expect(randomHolderAmounts).to.deep.equal(randomHoldersAfterUpgrade);
+
 
     });
   });
