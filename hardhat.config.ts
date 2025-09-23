63f2b56031c93cb69f9cf02a7c701aae19e9bed2
Whytecrowe <dor7an@gmail.com>
2025-09-23 16:13:03 -0700

    update packages and HH config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,8 +8,6 @@ import "@openzeppelin/hardhat-upgrades";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-ethers";
 import "solidity-coverage";
-import "./tasks/merkle";
-import "./tasks/deploy";
 
 task("accounts", "Prints the list of accounts", async (args, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -42,23 +40,23 @@ const config: HardhatUserConfig = {
     timeout: 50000,
   },
   networks: {
-    kovan: {
-      accounts: { mnemonic: process.env.TESTNET_MNEMONIC || "" },
-      url: `https://kovan.infura.io/v3/0e6434f252a949719227b5d68caa2657`,
-    },
-    ropsten: {
-      accounts: { mnemonic: process.env.TESTNET_MNEMONIC || "" },
-      url: "https://ropsten.infura.io/v3/77c3d733140f4c12a77699e24cb30c27",
-    },
-    rinkeby: {
-      accounts: { mnemonic: process.env.TESTNET_MNEMONIC || "" },
-      url: "https://rinkeby.infura.io/v3/77c3d733140f4c12a77699e24cb30c27",
-    },
-    sepolia: {
-      chainId: 11155111,
-      accounts: [`${process.env.TESTNET_PRIVATE_KEY}`],
-      url: `${process.env.SEPOLIA_RPC_URL}`,
-    },
+    // kovan: {
+    //   accounts: { mnemonic: process.env.TESTNET_MNEMONIC || "" },
+    //   url: `https://kovan.infura.io/v3/0e6434f252a949719227b5d68caa2657`,
+    // },
+    // ropsten: {
+    //   accounts: { mnemonic: process.env.TESTNET_MNEMONIC || "" },
+    //   url: "https://ropsten.infura.io/v3/77c3d733140f4c12a77699e24cb30c27",
+    // },
+    // rinkeby: {
+    //   accounts: { mnemonic: process.env.TESTNET_MNEMONIC || "" },
+    //   url: "https://rinkeby.infura.io/v3/77c3d733140f4c12a77699e24cb30c27",
+    // },
+    // sepolia: {
+    //   chainId: 11155111,
+    //   accounts: [`${process.env.TESTNET_PRIVATE_KEY}`],
+    //   url: `${process.env.SEPOLIA_RPC_URL}`,
+    // },
     localhost: {
       gas: "auto",
       gasPrice: "auto",
