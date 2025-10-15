c5068cac97e63a3bff3ed225059add13030fdaa4
James Earle <james@zero.tech>
2025-10-15 17:48:01 -0400

    remove console import

--- contracts/ZeroDAOTokenV2.sol
+++ contracts/ZeroDAOTokenV2.sol
@@ -12,8 +12,6 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-import {console} from "hardhat/console.sol";
-
 contract ZeroDAOTokenV2 is
   OwnableUpgradeable,
   ERC20Upgradeable,
